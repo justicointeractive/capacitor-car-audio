1e9c5c39d16fa2ef77d1b47197fb354db0b304b8
Joe Flateau <joe@joeflateau.net>
2021-09-29 10:23:33 -0400

    onGetRoot can be called before AudioPluginService is bound

--- android/src/main/java/com/justicointeractive/plugins/capacitor/caraudio/CarAudioMediaBrowserService.java
+++ android/src/main/java/com/justicointeractive/plugins/capacitor/caraudio/CarAudioMediaBrowserService.java
@@ -33,10 +33,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarAudioMediaBrowserService extends MediaBrowserServiceCompat {
-
     AudioPluginService service;
     RequestQueue requestQueue;
-    private boolean hasSentCarAppLaunchEvent = false;
+    
+    private boolean androidAutoHasLaunched = false;
 
     @Override
     public void onCreate() {
@@ -46,33 +46,36 @@ public class CarAudioMediaBrowserService extends MediaBrowserServiceCompat {
 
         Intent serviceIntent = new Intent(this.getApplicationContext(), AudioPluginService.class);
         this.getApplicationContext()
-            .bindService(
-                serviceIntent,
-                new ServiceConnection() {
-                    @Override
-                    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                        AudioPluginService.AudioPluginServiceBinder binder = (AudioPluginService.AudioPluginServiceBinder) iBinder;
-                        service = binder.getService();
-                        CarAudioMediaBrowserService.this.setSessionToken(service.mediaSession.getSessionToken());
-                    }
+                .bindService(
+                        serviceIntent,
+                        new ServiceConnection() {
+                            @Override
+                            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                                AudioPluginService.AudioPluginServiceBinder binder = (AudioPluginService.AudioPluginServiceBinder) iBinder;
+                                service = binder.getService();
+                                CarAudioMediaBrowserService.this.setSessionToken(service.mediaSession.getSessionToken());
+                                if (androidAutoHasLaunched) {
+                                    sendAndroidAutoLaunchEvent();
+                                }
+                            }
 
-                    @Override
-                    public void onServiceDisconnected(ComponentName componentName) {
-                        service = null;
-                    }
-                },
-                Context.BIND_AUTO_CREATE
-            );
+                            @Override
+                            public void onServiceDisconnected(ComponentName componentName) {
+                                service = null;
+                            }
+                        },
+                        Context.BIND_AUTO_CREATE
+                );
     }
 
     @Nullable
     @Override
     public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid, @Nullable Bundle rootHints) {
-        if (clientPackageName.equals("com.google.android.projection.gearhead") && !hasSentCarAppLaunchEvent) {
-            Bundle carAppLaunchEventParameters = new Bundle();
-            carAppLaunchEventParameters.putString("car_app_type", "Android Auto");
-            service.logEventIfFirebaseAnalyticsIsAvailable("car_app_launch", carAppLaunchEventParameters);
-            hasSentCarAppLaunchEvent = true;
+        if (clientPackageName.equals("com.google.android.projection.gearhead") && !androidAutoHasLaunched) {
+            androidAutoHasLaunched = true;
+            if (service != null) {
+                sendAndroidAutoLaunchEvent();
+            }
         }
         return new MediaBrowserServiceCompat.BrowserRoot("0", null);
     }
@@ -87,42 +90,42 @@ public class CarAudioMediaBrowserService extends MediaBrowserServiceCompat {
 
         if (parentId.equals("0")) {
             JsonObjectRequest request = new JsonObjectRequest(
-                Request.Method.GET,
-                rootUrl,
-                null,
-                response -> {
-                    new Thread(() -> {
-                        try {
-                            JSONArray tabs = response.getJSONArray("items");
-                            ArrayList<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
-                            for (int i = 0; i < tabs.length(); i++) {
-                                JSONObject tab = tabs.getJSONObject(i);
-                                int resourceId = this.getResources().getIdentifier(tab.getString("icon"), "drawable", this.getPackageName());
-                                Uri iconContentUri = new Uri.Builder()
-                                        .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-                                        .authority(this.getPackageName())
-                                        .appendPath(String.valueOf(resourceId))
-                                        .build();
-                                mediaItems.add(
-                                        new MediaBrowserCompat.MediaItem(
-                                                new MediaDescriptionCompat.Builder()
-                                                        .setMediaId(tab.getString("url"))
-                                                        .setTitle(tab.getString("title"))
-                                                        .setIconUri(iconContentUri)
-                                                        .build(),
-                                                MediaBrowserCompat.MediaItem.FLAG_BROWSABLE
-                                        )
-                                );
+                    Request.Method.GET,
+                    rootUrl,
+                    null,
+                    response -> {
+                        new Thread(() -> {
+                            try {
+                                JSONArray tabs = response.getJSONArray("items");
+                                ArrayList<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
+                                for (int i = 0; i < tabs.length(); i++) {
+                                    JSONObject tab = tabs.getJSONObject(i);
+                                    int resourceId = this.getResources().getIdentifier(tab.getString("icon"), "drawable", this.getPackageName());
+                                    Uri iconContentUri = new Uri.Builder()
+                                            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                                            .authority(this.getPackageName())
+                                            .appendPath(String.valueOf(resourceId))
+                                            .build();
+                                    mediaItems.add(
+                                            new MediaBrowserCompat.MediaItem(
+                                                    new MediaDescriptionCompat.Builder()
+                                                            .setMediaId(tab.getString("url"))
+                                                            .setTitle(tab.getString("title"))
+                                                            .setIconUri(iconContentUri)
+                                                            .build(),
+                                                    MediaBrowserCompat.MediaItem.FLAG_BROWSABLE
+                                            )
+                                    );
+                                }
+                                result.sendResult(mediaItems);
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
-                            result.sendResult(mediaItems);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }).start();
-                },
-                error -> {
-                    error.printStackTrace();
-                }
+                        }).start();
+                    },
+                    error -> {
+                        error.printStackTrace();
+                    }
             );
             requestQueue.add(request);
         } else {
@@ -134,87 +137,93 @@ public class CarAudioMediaBrowserService extends MediaBrowserServiceCompat {
                 return;
             }
             JsonObjectRequest request = new JsonObjectRequest(
-                Request.Method.GET,
-                url.toString(),
-                null,
-                response -> {
-                    new Thread(() -> {
-                    try {
-                        JSONArray groups = response.getJSONArray("items");
-                        ArrayList<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
-                        for (int groupIndex = 0; groupIndex < groups.length(); groupIndex++) {
-                            JSONObject group = groups.getJSONObject(groupIndex);
-                            JSONArray items = group.getJSONArray("items");
-                            for (int itemIndex = 0; itemIndex < items.length(); itemIndex++) {
-                                JSONObject item = items.getJSONObject(itemIndex);
+                    Request.Method.GET,
+                    url.toString(),
+                    null,
+                    response -> {
+                        new Thread(() -> {
+                            try {
+                                JSONArray groups = response.getJSONArray("items");
+                                ArrayList<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
+                                for (int groupIndex = 0; groupIndex < groups.length(); groupIndex++) {
+                                    JSONObject group = groups.getJSONObject(groupIndex);
+                                    JSONArray items = group.getJSONArray("items");
+                                    for (int itemIndex = 0; itemIndex < items.length(); itemIndex++) {
+                                        JSONObject item = items.getJSONObject(itemIndex);
 
-                                String itemUrl = item.has("url") ? item.getString("url") : null;
+                                        String itemUrl = item.has("url") ? item.getString("url") : null;
 
-                                if (itemUrl == null || itemUrl.length() == 0) {
-                                    continue;
-                                }
+                                        if (itemUrl == null || itemUrl.length() == 0) {
+                                            continue;
+                                        }
 
-                                MediaDescriptionCompat.Builder builder = new MediaDescriptionCompat.Builder()
-                                    .setTitle(item.getString("title"))
-                                    .setMediaId(itemUrl);
+                                        MediaDescriptionCompat.Builder builder = new MediaDescriptionCompat.Builder()
+                                                .setTitle(item.getString("title"))
+                                                .setMediaId(itemUrl);
 
-                                try {
-                                    if (!item.isNull("imageUrl")) {
-                                        String imageUrl = item.getString("imageUrl");
+                                        try {
+                                            if (!item.isNull("imageUrl")) {
+                                                String imageUrl = item.getString("imageUrl");
 
-                                        Uri imageFileUri = new Uri.Builder()
-                                                .scheme(ContentResolver.SCHEME_CONTENT)
-                                                .authority(this.getPackageName() + ".caraudiofileprovider")
-                                                .appendPath("imagefetch")
-                                                .appendPath(imageUrl)
-                                                .build();
+                                                Uri imageFileUri = new Uri.Builder()
+                                                        .scheme(ContentResolver.SCHEME_CONTENT)
+                                                        .authority(this.getPackageName() + ".caraudiofileprovider")
+                                                        .appendPath("imagefetch")
+                                                        .appendPath(imageUrl)
+                                                        .build();
 
-                                        builder.setIconUri(imageFileUri);
+                                                builder.setIconUri(imageFileUri);
+                                            }
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+
+                                        Bundle extras = new Bundle();
+                                        extras.putString(MediaConstants.DESCRIPTION_EXTRAS_KEY_CONTENT_STYLE_GROUP_TITLE, group.getString("title"));
+                                        builder.setExtras(extras);
+
+                                        switch (item.getString("type")) {
+                                            case "upcoming":
+                                                continue;
+                                            case "playable":
+                                                extras.putString("src", item.getString("url"));
+                                                extras.putString("title", item.getString("title"));
+                                                extras.putString("artist", item.getString("description"));
+                                                extras.putString("artwork", item.getString("artworkUrl"));
+                                                extras.putString("itemId", item.getString("itemId"));
+                                                extras.putString("contentType", item.getString("contentType"));
+                                                extras.putString("car_app_type", "Android Auto");
+                                                mediaItems.add(
+                                                        new MediaBrowserCompat.MediaItem(builder.build(), MediaBrowserCompat.MediaItem.FLAG_PLAYABLE)
+                                                );
+                                                break;
+                                            case "browsable":
+                                                mediaItems.add(
+                                                        new MediaBrowserCompat.MediaItem(builder.build(), MediaBrowserCompat.MediaItem.FLAG_BROWSABLE)
+                                                );
+                                                break;
+                                        }
                                     }
-                                } catch (Exception e) {
-                                    e.printStackTrace();
                                 }
-
-                                Bundle extras = new Bundle();
-                                extras.putString(MediaConstants.DESCRIPTION_EXTRAS_KEY_CONTENT_STYLE_GROUP_TITLE, group.getString("title"));
-                                builder.setExtras(extras);
-
-                                switch (item.getString("type")) {
-                                    case "upcoming":
-                                        continue;
-                                    case "playable":
-                                        extras.putString("src", item.getString("url"));
-                                        extras.putString("title", item.getString("title"));
-                                        extras.putString("artist", item.getString("description"));
-                                        extras.putString("artwork", item.getString("artworkUrl"));
-                                        extras.putString("itemId", item.getString("itemId"));
-                                        extras.putString("contentType", item.getString("contentType"));
-                                        extras.putString("car_app_type", "Android Auto");
-                                        mediaItems.add(
-                                            new MediaBrowserCompat.MediaItem(builder.build(), MediaBrowserCompat.MediaItem.FLAG_PLAYABLE)
-                                        );
-                                        break;
-                                    case "browsable":
-                                        mediaItems.add(
-                                            new MediaBrowserCompat.MediaItem(builder.build(), MediaBrowserCompat.MediaItem.FLAG_BROWSABLE)
-                                        );
-                                        break;
-                                }
+                                result.sendResult(mediaItems);
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
-                        }
-                        result.sendResult(mediaItems);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+
+
+                        }).start();
+                    },
+                    error -> {
+                        error.printStackTrace();
                     }
-
-
-                    }).start();
-                },
-                error -> {
-                    error.printStackTrace();
-                }
             );
             requestQueue.add(request);
         }
+    }
+
+    private void sendAndroidAutoLaunchEvent() {
+        Bundle carAppLaunchEventParameters = new Bundle();
+        carAppLaunchEventParameters.putString("car_app_type", "Android Auto");
+        service.logEventIfFirebaseAnalyticsIsAvailable("car_app_launch", carAppLaunchEventParameters);
     }
 }
